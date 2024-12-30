bebd264460fd6136689cbcf3b79d5c577e23da4e
Andrew Piroli <drewpiroli@gmail.com>
2024-12-30 12:32:30 -0500

    Print negotiated TLS info in sample app

--- sample-app/source/main.c
+++ sample-app/source/main.c
@@ -50,6 +50,23 @@ static size_t cb(void *data, size_t size, size_t nmemb, void *clientp) {
 	return realsize;
 }
 
+static int verbose_print(CURL *handle, curl_infotype type, char *data, size_t size, void *clientp) {
+	(void)handle;
+	(void)data;
+	(void)clientp;
+
+	if (type == CURLINFO_TEXT && memcmp(data, "mbedTLS", 7) == 0) {
+		char* nullterminated = malloc(size + 1);
+		if (!nullterminated)
+			return 0;
+		memcpy(nullterminated, data, size);
+		nullterminated[size] = '\0';
+		printf("%s", nullterminated);
+		free(nullterminated);
+	}
+	return 0;
+}
+
 char* http_head(char* url, char* err) {
 	CURL* curl = curl_easy_init();
 	if (!curl){
@@ -59,15 +76,24 @@ char* http_head(char* url, char* err) {
 	struct memory chunk = {0};
 	chunk.response = malloc(1);
 	chunk.size = 0;
-	curl_easy_setopt(curl, CURLOPT_SSLCERTTYPE, "PEM");
-	curl_easy_setopt(curl, CURLOPT_CAINFO_BLOB, &ca_info);
 	curl_easy_setopt(curl, CURLOPT_URL, url);
 	curl_easy_setopt(curl, CURLOPT_PROTOCOLS_STR, "HTTP,HTTPS");
 	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, err);
-	curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+	curl_easy_setopt(curl, CURLOPT_NOBODY, 1); // Tells curl to make a HEAD request, not GET.
 	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, cb);
 	curl_easy_setopt(curl, CURLOPT_HEADERDATA, (void*)&chunk);
+	if (strncmp(url, "https", 5) == 0) {
+		curl_easy_setopt(curl, CURLOPT_SSLCERTTYPE, "PEM");
+		curl_easy_setopt(curl, CURLOPT_CAINFO_BLOB, &ca_info);
+		// I wanted to just print TLS version and cipher...
+		// There is a built in curl_easy_getinfo for that, but I couldn't get it to work properly.
+		// So we just enable the built in verbose output, and set a custom function to only print lines starting with "mbedTLS"
+		// This happens to print the info I want.
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+		curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, verbose_print);
+	}
 	CURLcode res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
 	if (res == CURLE_OK) {
 		return chunk.response;
 	}
@@ -124,12 +150,13 @@ int main(void) {
 	if (rmode->viTVMode & VI_NON_INTERLACE) {
 		VIDEO_WaitVSync();
 	}
-	SYS_SetResetCallback(timetostop);
+	SYS_SetResetCallback((resetcallback)timetostop);
 	SYS_SetPowerCallback(timetostop);
 
 	printf("libcurl version: %s\n", curl_version());
 
-	// try a few times to initialize libwiisocket (?)
+	// The original documentation for libwiisocket showed using multiple tries to init and get an ip.
+	// I don't think this is necessary, but I'm leaving it in just in case.
 	int socket_init_success = -1;
 	for (int attempts = 0;attempts < 3;attempts++) {
 		socket_init_success = wiisocket_init();
@@ -141,7 +168,7 @@ int main(void) {
 		puts("failed to init wiisocket");
 		goto loop;
 	}
-	// try a few times to get an ip (?)
+
 	u32 ip = 0;
 	for (int attempts = 0; attempts < 3; attempts++) {
 		ip = gethostid();
